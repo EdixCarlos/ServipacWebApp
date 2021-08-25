39f297546ca1c7b6e9f7d1b70a6da9328791d868
Edix Carlos <edix.carlos@tecsup.edu.pe>
2021-08-24 20:54:40 -0500

    creacion de proyectos

---
+++ ProyectReact/servipac/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h2>Hola</h2>    
+    </div>
+  )
+}
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
+export default App
